0b15c2a261ed3983848f091a0d7a9887a3eab279
SaraPerezSoler <saraperezsoler@gmail.com>
2016-11-16 15:45:51 +0100

    borrando innecesarios

--- es.uam.sara.tfg.ast3/src/Hola.java
+++
@@ -1,8 +0,0 @@
-
-public enum Hola {
-	Casa(0), Hotel(1);
-	int i;
-	private Hola (int i){
-		this.i=i;
-	}
-}
--- es.uam.sara.tfg.ast3/src/es/uam/sara/tfg/properties/annotations/AnnotationDeclaration.java
+++
@@ -1,9 +0,0 @@
-package es.uam.sara.tfg.properties.annotations;
-
-import org.eclipse.jdt.core.dom.AnnotationTypeDeclaration;
-
-import es.uam.sara.tfg.properties.Properties;
-
-public abstract class  AnnotationDeclaration extends Properties<AnnotationTypeDeclaration>{
-
-}
